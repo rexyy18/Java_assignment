d3c4636b964c8af7858fad1c2910a111e0c309c2
rexyy18 <rtnyarko003@st.ug.edu.gh>
2026-02-05 01:33:08 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        executeGradeReport(77);
+
+    }
+    public static void executeGradeReport(double score){
+        if (validateScore(score)){
+            System.out.println("Invalid score");
+            return;
+        }
+    char grade = calculateLetterGrade(score);
+    displayPerformanceMessage(grade);
+
+    }
+    public static boolean validateScore(double score){
+        return score < 0 || score > 100;
+        }
+    public static char calculateLetterGrade(double score) {
+        if (score >= 90) {
+            return 'A';
+        } else if (score>= 80) {
+            return 'B';
+        } else if (score >= 70) {
+            return 'C';
+        } else if (score >= 60) {
+            return 'D';
+        } else if (score >= 50) {
+            return 'E';
+        }
+        return 'F';
+    }
+    public static void displayPerformanceMessage(char grade) {
+        if (grade == 'A') {
+            System.out.println("Execellent");;
+        }else if (grade == 'B') {
+            System.out.println("You have done well");
+        }else if (grade == 'C') {
+            System.out.println("Keep trying");
+        }else if (grade == 'D') {
+            System.out.println("More room for improvement");
+        }else if (grade == 'E') {
+            System.out.println("Learn Hard");
+        }else if (grade == 'F') {
+            System.out.println("See me!!!");
+        }
+
+    }
+
+    }
+
+
